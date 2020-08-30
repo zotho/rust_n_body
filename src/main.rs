f55fad335d4bd6f2272545479e3df0638c29578e
Zotho <svjatoslavalekseef2@gmail.com>
2020-08-30 16:40:28 +0500

    Performance improvements. Add raindow_trail feature

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,23 @@ use vecmath::*;
 use itertools::Itertools;
 use rand::Rng;
 
+#[cfg(feature = "raindow_trail")]
+const COLORS: [Color; 25] = [LIGHTGRAY, GRAY, DARKGRAY, YELLOW, GOLD, ORANGE, PINK, RED, MAROON, GREEN, LIME, DARKGREEN, SKYBLUE, BLUE, DARKBLUE, PURPLE, VIOLET, DARKPURPLE, BEIGE, BROWN, DARKBROWN, WHITE, BLACK, BLANK, MAGENTA];
+
 const NANO_MULT: f64 = 1000000000.0;
 
-const G: f64 = 5.0;
-const GRAV_POWER: f64 = 2.0;
-// const G: f64 = 1000.0;
-// const GRAV_POWER: f64 = 3.0;
+const DRAW_END: bool = true;
+const TRAIL_END: usize = 100;
+
+// const G: f64 = 5.0;
+// const GRAV_POWER: f64 = 2.0;
+const G: f64 = 1000.0;
+const GRAV_POWER: i32 = 3;
+
+const MAX_INV_LEN: f64 = 1.0;
+
+const FIXED_DT: bool = true;
+const DT: f64 = 0.004;
 
 fn lerp<T: Copy + Add<Output = T> + Mul<Output = T> + Sub<Output = T>>(v0: T, v1: T, t: T) -> T {
     v0 + t * (v1 - v0)
@@ -32,7 +43,7 @@ struct Particle {
     pos: Vector2<f64>,
     vel: Vector2<f64>,
     mass: f64,
-    trail: VecDeque<(Vector2<f64>, f32)>,
+    trail: VecDeque<(Vector2<f64>, f32, bool)>,
 }
 
 impl Particle {
@@ -46,38 +57,94 @@ impl Particle {
     }
 
     fn draw(self: &Self) {
-        let trail_len = self.trail.len();
-        let trail_end = 100;
 
-        if trail_len > trail_end {
-            for i in 1..trail_len {
-                let ([x1, y1], _s1) = self.trail[i - 1];
-                let ([x2, y2], s2) = self.trail[i];
-
-                if i == trail_end {
-                    let sides = (s2.powf(0.8).ceil() as u8).max(3);
-                    draw_poly(x2 as f32, y2 as f32, sides, s2, 0.0, if self.mass < 0.0 {GREEN} else {WHITE});
-                    break;
+        if DRAW_END {
+            let size = self.size();
+            draw_poly(self.pos[0] as f32, self.pos[1] as f32, self.sides(), size, 0.0, WHITE);
+        } else {
+            let trail_len = self.trail.len();
+            if trail_len > TRAIL_END {
+                for i in 1..trail_len {
+                    let ([x1, y1], _, _) = self.trail[i - 1];
+                    let ([x2, y2], s2, _) = self.trail[i];
+    
+                    if i == TRAIL_END {
+                        draw_poly(x2 as f32, y2 as f32, self.sides(), s2, 0.0, WHITE);
+                        break;
+                    }
+    
+                    draw_line(x1 as f32, y1 as f32, x2 as f32, y2 as f32, 1.0, RED);
                 }
-
-                draw_line(x1 as f32, y1 as f32, x2 as f32, y2 as f32, 1.0, RED);
             }
         }
-        //  else {
-        //     let [x, y] = self.pos;
-        //     let size = self.size();
-        //     draw_poly(x as f32, y as f32, (size.ceil() as u8).max(3), size, 0.0, WHITE);
-        // }
+    }
+
+    #[cfg(feature = "raindow_trail")]
+    fn draw_trail(self: &Self) {
+        let len = self.trail.len();
+        for i in 1..len {
+            let ([x1, y1], _, _) = self.trail[i - 1];
+            let ([x2, y2], _, _) = self.trail[i];
+
+            draw_line(x1 as f32, y1 as f32, x2 as f32, y2 as f32, 2.0, COLORS[(len - i) % COLORS.len()]);
+        }
+    }
+
+    #[cfg(not(feature = "raindow_trail"))]
+    fn draw_trail(self: &Self) {
+        let len = self.trail.len();
+        for i in 1..len {
+            let ([x1, y1], _, _) = self.trail[i - 1];
+            let ([x2, y2], _, _) = self.trail[i];
+
+            draw_line(x1 as f32, y1 as f32, x2 as f32, y2 as f32, 1.0, RED);
+        }
+    }
+
+    fn optimize_trail(self: &mut Self) -> usize {
+        let n_iter = self.trail.len() / 2;
+
+        let mut retain_total = 0;
+        // let mut last_retain = 0;
+        for j in 10..n_iter {
+            let i = j * 2;
+            let ([x0, y0], _, _) = self.trail[i - 2];
+            let ([x1, y1], _, _) = self.trail[i - 1];
+            let ([x2, y2], _, _) = self.trail[i];
+            let d0 = vec2(x1 as f32 - x0 as f32, y1 as f32 - y0 as f32);
+            let d1 = vec2(x2 as f32 - x1 as f32, y2 as f32 - y1 as f32);
+
+            let (l0, l1) = (d0.length(), d1.length());
+            let prod = d0.dot(d1) / (l0 * l1);
+
+            let sum = (l0 + l1) / 3.0;
+
+            let need_retain = prod > 0.99 && l0 > sum && l1 > sum && sum < 10.0;
+            if need_retain {
+                // last_retain = i;
+                retain_total += 1;
+            }
+            self.trail[i - 1].2 = !need_retain;
+        }
+        // println!("{:}\n", std::str::from_utf8(self.trail.iter().map(|(_, _, r)| if *r {43} else {45}).collect::<Vec<u8>>().as_slice()).unwrap());
+        // println!("{:>5}/{:>5}", last_retain, self.trail.len());
+        self.trail.retain(|(_, _, retain)| *retain);
+
+        retain_total
     }
 
     fn update_pos(self: &mut Self, dt: f64) {
         self.pos = vec2_add(self.pos, vec2_scale(self.vel, dt));
         let size = self.size();
-        self.trail.push_front((self.pos, size));
+        self.trail.push_front((self.pos, size, true));
     }
 
     fn size(self: &Self) -> f32 {
         (self.mass as f32).powf(1.0 / 3.0)
+    }
+
+    fn sides(self: &Self) -> u8 {
+        (self.size().powf(0.8).ceil() as u8).max(3)
     }
 }
 
@@ -104,12 +171,16 @@ async fn main() {
     let mut mouse_pos_prev;
     let mut mouse_vel;
 
-    let mut n_init_on_step = 100;
-    let mut n_updates = 1;
+    let mut n_init_on_step = 0;
+    
+    let mut n_updates: usize = 100;
     let mut time_speed = 1.0;
 
-    let init_size = 100;
-    let init_speed = 100.0;
+    let mut draw_debug = false;
+
+    let init_size = 50;
+    let init_speed = 20.0;
+    let init_radius = 100.0;
     
     let mut particles = Vec::with_capacity(init_size);
 
@@ -117,7 +188,7 @@ async fn main() {
         let k = i as f64 / init_size as f64 * 2.0 * std::f64::consts::PI;
 
         let mut p = Particle::new();
-        p.pos = [k.cos() * 100.0 + width as f64 / 2.0, k.sin() * 100.0 + height as f64 / 2.0];
+        p.pos = [k.cos() * init_radius + width as f64 / 2.0, k.sin() * init_radius + height as f64 / 2.0];
         p.vel = [(k + std::f64::consts::PI / 2.0).cos() * init_speed, (k + std::f64::consts::PI / 2.0).sin() * init_speed];
         particles.push(p);
     }
@@ -125,7 +196,7 @@ async fn main() {
     loop {
         width = screen_width();
         height = screen_height();
-        
+
         nanoseconds = now.elapsed().as_nanos() as f64;
         let dt = nanoseconds / NANO_MULT;
         let fps = NANO_MULT / nanoseconds;
@@ -149,10 +220,10 @@ async fn main() {
         if is_key_pressed(KeyCode::Right) {
             time_speed *= 1.1;
         }
-        if is_key_pressed(KeyCode::Down) {
-            n_updates = 0.max(n_updates - 1);
+        if is_key_down(KeyCode::Down) {
+            n_updates = n_updates.checked_sub(1).unwrap_or(0);
         }
-        if is_key_pressed(KeyCode::Up) {
+        if is_key_down(KeyCode::Up) {
             n_updates += 1;
         }
         if is_key_down(KeyCode::Minus) {
@@ -160,6 +231,9 @@ async fn main() {
         }
         if is_key_down(KeyCode::Equal) {
             n_init_on_step += 1;
+        }
+        if is_key_pressed(KeyCode::D) {
+            draw_debug = !draw_debug;
         }
 
         if is_mouse_button_down(MouseButton::Left) {
@@ -192,12 +266,17 @@ async fn main() {
 
         clear_background(GRAY);
 
-        let (x, y) = mouse_pos;
-        draw_circle(x, y, 10.0, WHITE);
+        // let (x, y) = mouse_pos;
+        // draw_circle(x, y, 10.0, WHITE);
 
         let mut particles_time_vel = 0;
         for _ in 0..n_updates {
-            let dt = dt * time_speed / n_updates as f64;
+            let current_dt;
+            if FIXED_DT {
+                current_dt = DT;
+            } else {
+                current_dt = dt * time_speed / n_updates as f64;
+            }
 
             let particles_now_vel = Instant::now();
             (0..particles.len()).tuple_combinations().for_each(|(i1, i2)| {
@@ -210,15 +289,15 @@ async fn main() {
     
                 let d12 = vec2_sub(p2.pos, p1.pos);
                 let inv_len = vec2_inv_len(d12);
-                if inv_len > 0.05 {
+                if inv_len * (p1.mass * p2.mass).powf(1.0 / GRAV_POWER as f64) > MAX_INV_LEN {
                     p1.pos = lerp_mass_vec(p1.pos, p2.pos, p1.mass, p2.mass);
                     p1.vel = lerp_mass_vec(p1.vel, p2.vel, p1.mass, p2.mass);
                     p1.mass = p1.mass + p2.mass;
                     p2.mass = -0.00001;
                 } else {
-                    let a = vec2_scale(d12, G * inv_len.powf(GRAV_POWER));
-                    p1.vel = vec2_add(p1.vel, vec2_scale(a, dt * p2.mass));
-                    p2.vel = vec2_add(p2.vel, vec2_scale(a, -dt * p1.mass));
+                    let a = vec2_scale(d12, G * inv_len.powi(GRAV_POWER));
+                    p1.vel = vec2_add(p1.vel, vec2_scale(a, current_dt * p2.mass));
+                    p2.vel = vec2_add(p2.vel, vec2_scale(a, -current_dt * p1.mass));
                 }
             });
             particles_time_vel += particles_now_vel.elapsed().as_micros();
@@ -229,27 +308,49 @@ async fn main() {
             });
     
             particles.iter_mut().for_each(|p| {
-                p.update_pos(dt);
+                p.update_pos(current_dt);
                 // p.vel = vec2_scale(p.vel, 1.0001);
             });
         }
 
-        let particles_time = particles_now.elapsed().as_micros();
+        let particles_now_retain = Instant::now();
+        let mut retain_count = 0;
+        particles.iter_mut().for_each(|p| {
+            retain_count += p.optimize_trail();
+        });
+        let particles_time_retain = particles_now_retain.elapsed().as_micros();
 
+        let particles_now_trail = Instant::now();
+        particles.iter().for_each(|p| {
+            p.draw_trail();
+        });
+        let particles_time_trail = particles_now_trail.elapsed().as_micros();
+
+        let particles_now_draw = Instant::now();
         particles.iter().for_each(|p| {
             p.draw();
         });
+        let particles_time_draw = particles_now_draw.elapsed().as_micros();
 
-        text_line(format!("N PARTICLES: {}", particles.len()).as_str(), 1);
-        text_line(format!("FPS: {:.1} (MIN: {:.1})", fps, min_fps).as_str(), 2);
-        text_line(format!("ALL TIME:  {:>7}", nanoseconds as u128 / 1000).as_str(), 3);
-        text_line(format!("PARTICLES: {:>7} ({} - {})", particles_time, particles_time_vel, particles_time_vel as f32 / particles_time as f32).as_str(), 4);
-        text_line(format!("REST:      {:>7}", nanoseconds as u128  / 1000 - particles_time).as_str(), 5);
-        text_line(format!("MASSES: {:?}", particles.iter().map(|p| p.mass).collect::<Vec<f64>>()).as_str(), 6);
-        text_line(format!("TRAILS: {:?}", particles.iter().map(|p| p.trail.len()).collect::<Vec<usize>>()).as_str(), 7);
-        text_line(format!("SPEED: {}", time_speed).as_str(), 8);
-        text_line(format!("UPDATE STEPS: {}", n_updates).as_str(), 9);
-        text_line(format!("N INIT NEW PARTICLES: {}", n_init_on_step).as_str(), 10);
+        let particles_time = particles_now.elapsed().as_micros();
+
+        if draw_debug {
+            text_line(format!("N PARTICLES: {}", particles.len()).as_str(), 1);
+            text_line(format!("FPS: {:.1} (MIN: {:.1})", fps, min_fps).as_str(), 2);
+            text_line(format!("ALL TIME:  {:>7}", nanoseconds as u128 / 1000).as_str(), 3);
+            text_line(format!("PARTICLES: {:>7}", particles_time).as_str(), 4);
+            text_line(format!("         - {:>7} grav   ({:.3} part)", particles_time_vel, particles_time_vel as f32 / particles_time as f32).as_str(), 5);
+            text_line(format!("         - {:>7} retain ({:.3} part)", particles_time_retain, particles_time_retain as f32 / particles_time as f32).as_str(), 6);
+            text_line(format!("         - {:>7} trail  ({:.3} part)", particles_time_trail, particles_time_trail as f32 / particles_time as f32).as_str(), 7);
+            text_line(format!("         - {:>7} draw   ({:.3} part)", particles_time_draw, particles_time_draw as f32 / particles_time as f32).as_str(), 8);
+            text_line(format!("REST:      {:>7}", (nanoseconds as u128  / 1000).checked_sub(particles_time).unwrap_or(0)).as_str(), 9);
+            text_line(format!("MASSES: {:?}", particles.iter().map(|p| p.mass).collect::<Vec<f64>>()).as_str(), 10);
+            text_line(format!("TRAILS: {:?}", particles.iter().map(|p| p.trail.len()).sum::<usize>()).as_str(), 11);
+            text_line(format!("SPEED: {}", time_speed).as_str(), 12);
+            text_line(format!("UPDATE STEPS: {}", n_updates).as_str(), 13);
+            text_line(format!("N INIT NEW PARTICLES: {}", n_init_on_step).as_str(), 14);
+            text_line(format!("N RETAINED: {}", retain_count).as_str(), 15);
+        }
 
         next_frame().await;
     }
